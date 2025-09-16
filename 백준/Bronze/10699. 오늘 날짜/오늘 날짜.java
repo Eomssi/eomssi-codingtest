35517812f4eb37c8da15d2fa5aed923c1968e51f
Eomssi <88015264+Eomssi@users.noreply.github.com>
2025-09-16 11:45:23 +0900

    [Bronze V] Title: 오늘 날짜, Time: 216 ms, Memory: 18548 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,13 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+        Date date = new Date();
+        String dateString = sdf.format(date);
+
+        System.out.println(dateString);
+    }
+}
